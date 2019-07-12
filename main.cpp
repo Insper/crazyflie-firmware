e0635a5784a00dc048dcad9692b261ca03745746
Fabio Bobrow <33038597+fbobrow@users.noreply.github.com>
2019-07-12 12:04:29 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 // Example programs
-//#include "programs/examples/ex01_leds.h"
+#include "programs/examples/ex01_leds.h"
 //#include "programs/examples/ex02_motors.h"
 //#include "programs/examples/ex03_serial.h"
-#include "programs/examples/ex04_imu.h"
+//#include "programs/examples/ex04_imu.h"
 //#include "programs/examples/ex05_range.h"
 //#include "programs/examples/ex06_flow.h"
 
