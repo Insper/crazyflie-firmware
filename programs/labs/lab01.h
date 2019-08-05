8ddb1c9286600353e3f6e045ea38a2fec0e32e7a
Fabio Bobrow <33038597+fbobrow@users.noreply.github.com>
2019-08-05 16:34:43 -0300

    Update lab01.h

--- programs/labs/lab01.h
+++ programs/labs/lab01.h
@@ -9,15 +9,15 @@
 int main()
 {
     // Blink blue LED indicating inicialization (5 seconds)
-
+    
     // Turn on red LEDs indicating motors are armed
-
+    
     // Test all motors with different frequencies (to make different noises)
-
+    
     // Turn off red LEDs indicating motors are disarmed
-
+    
     // Blink green LEDs indicating end of program
-    while(1)
+    while(true)
     {
         
     }
