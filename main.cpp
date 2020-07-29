9c9874129c999007f0eb55f3fc2097f36a75e2ee
fbobrow <fbob@me.com>
2020-07-29 18:05:10 -0300

    Update makefile

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 // Example programs
 #include "programs/examples/ex01_leds.h"
-//#include "programs/examples/ex02_motors.h"
-//#include "programs/examples/ex03_serial.h"
-//#include "programs/examples/ex04_imu.h"
-//#include "programs/examples/ex05_range.h"
-//#include "programs/examples/ex06_flow.h"
+// #include "programs/examples/ex02_motors.h"
+// #include "programs/examples/ex03_serial.h"
+// #include "programs/examples/ex04_imu.h"
+// #include "programs/examples/ex05_range.h"
+// #include "programs/examples/ex06_flow.h"
 
 // Lab programs
-//#include "programs/labs/lab01.h"
+// #include "programs/labs/lab01.h"
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 flash:
-	python3 -m cfloader  flash  BUILD/ARCH_MAX/ARMC6/crazyflie-firmware.bin stm32-fw
+	cfloader  flash  BUILD/ARCH_MAX/ARMC6/crazyflie-firmware.bin stm32-fw
