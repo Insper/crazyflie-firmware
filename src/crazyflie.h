5f3fe4065531835142246d0f24eed38f14f39d27
fbobrow <fbob@me.com>
2020-07-29 18:40:25 -0300

    Create Crazyflie 2.1 drivers

--- src/crazyflie.h
+++ src/crazyflie.h
@@ -5,9 +5,14 @@
 #include "utils/pin_names.h"
 #include "utils/parameters.h"
 
-// Crazyflie hardware abstraction layer
-#include "drivers/mpu9250.h"
-#include "drivers/vl53l0x.h"
-#include "drivers/pmw3901.h"
+// Crazyflie 2.0 hardware abstraction layer
+#include "drivers/mpu9250.h"    // IMU sensor
+#include "drivers/vl53l0x.h"    // Range sensor
+#include "drivers/pmw3901.h"    // Optical flow sensor
+
+// // Crazyflie 2.1 hardware abstraction layer
+// #include "drivers/bmi088.h"     // IMU sensor
+// #include "drivers/vl53l1x.h"    // Range sensor
+// #include "drivers/pmw3901.h"    // Optical flow sensor
 
 #endif
