7baed79acef9aff9b98496e10d1d2520cf9fa410
fbobrow <fbob@me.com>
2020-07-30 16:47:06 -0300

    Create BMI088 driver library

--- src/drivers/bmi088.cpp
+++ src/drivers/bmi088.cpp
@@ -1,1 +1,168 @@
+#include "bmi088.h"
+ 
+// Class constructor
+BMI088::BMI088(PinName sda, PinName scl) : i2c(sda, scl)
+{
+}
+ 
+// Initialize sensor (return true if connection succeeded)
+bool BMI088::init()
+{
+    // Setup I2C bus
+    setup_i2c();
+    // Test I2C connection
+    if (test_i2c()) 
+    {
+        // Setup accelerometer and gyroscope
+        setup_acc();
+        setup_gyr();
+        return true;
+    } 
+    else 
+    {
+        return false;
+    }
+}
+ 
+// Read sensor data
+void BMI088::read()
+{
+    // Read accelerometer and gyroscope data
+    read_acc();
+    read_gyr();
+}
+ 
+// Write data into register on given I2C bus address
+void BMI088::write_reg(uint8_t add, uint8_t reg, uint8_t data)
+{
+    // Register address and data that will be writed
+    char i2c_reg_data[2] = {reg, data};
+    // Point to register address and write data
+    i2c.write(add, i2c_reg_data, 2);
+}
+ 
+// Read data from register on given I2C bus address
+char BMI088::read_reg(uint8_t add, uint8_t reg)
+{
+    // Register address
+    char i2c_reg[1] = {reg};
+    // Data that will be readed
+    char i2c_data[1];
+    // Point to register address
+    i2c.write(add, i2c_reg, 1);
+    // Read data
+    i2c.read(add, i2c_data, 1);
+    // Return readed data
+    return i2c_data[0];
+}
 
+// Setup I2C bus
+void BMI088::setup_i2c()
+{
+    // Setup I2C bus frequency to 100kHz
+    i2c.frequency(100000);
+}
+ 
+// Test I2C connection
+bool BMI088::test_i2c()
+{
+    // Read accelerometer and gyroscope device identity and check if they are equal to 0x1E and 0x0F
+    return ((read_reg(ACC_ADD, ACC_CHIP_ID) == 0x1E) && (read_reg(GYR_ADD,GYR_CHIP_ID) == 0x0F));
+}
+ 
+// Setup accelerometer
+void BMI088::setup_acc(acc_range a_range, acc_odr a_odr)
+{
+    // Setup full-scale range
+    write_reg(ACC_ADD, ACC_RANGE, (0x00 << 2) | (a_range));
+    // Setup output data rate
+    write_reg(ACC_ADD, ACC_CONF, (0X0A << 4) | (a_odr));
+    // Setup active mode 
+    write_reg(ACC_ADD, ACC_PWR_CTRL, 0x04);
+    // Adjust resolution [m/s^2 / bit] accordingly to choosed full-scale range
+    switch (a_range) 
+    {
+        case ACC_RANGE_3G:
+            a_res =  3.0*(9.80665/32768.0);
+            break;
+        case ACC_RANGE_6G:
+            a_res =  6.0*(9.80665/32768.0);
+            break;
+        case ACC_RANGE_12G:
+            a_res =  12.0*(9.80665/32768.0);
+            break;
+        case ACC_RANGE_24G:
+            a_res = 24.0*(9.80665/32768.0);
+            break;
+    }
+}
+ 
+// Setup gyroscope
+void BMI088::setup_gyr(gyr_range g_range, gyr_odr g_odr)
+{
+    // Write configuration data
+    write_reg(GYR_ADD, GYR_RANGE, (0b000000 << 2) | (g_range));
+    // Write configuration data
+    write_reg(GYR_ADD, GYRO_BANDWIDTH, (0b100000 << 2) | (g_odr));
+    // Adjust resolution [rad/s / bit] accordingly to choose scale
+    switch (g_range) 
+    {
+        case GYR_RANGE_125DPS:
+            g_res =  125.0*((3.14159/180.0)/32768.0);
+            break;
+        case GYR_RANGE_250DPS:
+            g_res =  250.0*((3.14159/180.0)/32768.0);
+            break;
+        case GYR_RANGE_500DPS:
+            g_res = 500.0*((3.14159/180.0)/32768.0);
+            break;
+        case GYR_RANGE_1000DPS:
+            g_res = 1000.0*((3.14159/180.0)/32768.0);
+            break;
+        case GYR_RANGE_2000DPS:
+            g_res = 2000.0*((3.14159/180.0)/32768.0);
+            break;
+    }
+}
+ 
+// Read accelerometer output data
+void BMI088::read_acc()
+{
+    // Register address
+    char reg[1] = {ACC_X_LSB};
+    // Data that will be readed
+    char data[6];
+    // Point to register address
+    i2c.write(ACC_ADD, reg, 1);
+    // Read data from this address (register address will auto-increment and all three axis information (two 8 bit data each) will be read)
+    i2c.read(ACC_ADD, data, 6);
+    // Reassemble the data (two 8 bit data into one 16 bit data)
+    int16_t ax_raw = data[0] | (data[1] << 8);
+    int16_t ay_raw = data[2] | (data[3] << 8);
+    int16_t az_raw = data[4] | (data[5] << 8);
+    // Convert to SI units [m/s^2]
+    ax = -ax_raw*a_res;
+    ay = -ay_raw*a_res;
+    az = -az_raw*a_res;
+}
+ 
+// Read accelerometer data
+void BMI088::read_gyr()
+{
+    // Register address
+    char reg[1] = {GYR_X_LSB};
+    // Data that will be readed
+    char data[6];
+    // Point to register address
+    i2c.write(GYR_ADD, reg, 1);
+    // Read data from this address (register address will auto-increment and all three axis information (two 8 bit data each) will be read)
+    i2c.read(GYR_ADD, data, 6);
+    // Reassemble the data (two 8 bit data into one 16 bit data)
+    int16_t gx_raw = data[0] | (data[1] << 8);
+    int16_t gy_raw = data[2] | (data[3] << 8);
+    int16_t gz_raw = data[4] | (data[5] << 8);
+    // Convert to SI units [rad/s]
+    gx =  gx_raw*g_res;
+    gy =  gy_raw*g_res;
+    gz =  gz_raw*g_res;
+}
---
+++ src/drivers/bmi088.h
@@ -0,0 +1,119 @@
+#ifndef bmi088_h
+#define bmi088_h
+ 
+#include "mbed.h"
+ 
+// Accelerometer I2C bus address
+#define ACC_ADD 0b0011000 << 1  // Shift 1 bit left because mbed utilizes 8-bit addresses and not 7-bit
+// Accelerometer device identity register address 
+#define ACC_CHIP_ID 0x00
+// Accelerometer configuration registers addresses
+#define ACC_CONF 0x40
+#define ACC_RANGE 0x41
+#define ACC_PWR_CTRL 0x7D
+// Accerometer output register addresses
+#define ACC_X_LSB 0x12
+#define ACC_X_MSB 0x13
+#define ACC_Y_LSB 0x14
+#define ACC_Y_MSB 0x15
+#define ACC_Z_LSB 0x16
+#define ACC_Z_MSB 0x17
+
+// Gyroscope I2C bus address
+#define GYR_ADD 0b1101001 << 1  // Shift 1 bit left because mbed utilizes 8-bit addresses and not 7-bit
+// Gyroscope device identity  
+#define GYR_CHIP_ID 0x00
+// Gyroscope configuration registers addresses
+#define GYR_RANGE 0x0F
+#define GYRO_BANDWIDTH 0x10
+// Gyroscope output register addresses
+#define GYR_X_LSB 0x02
+#define GYR_X_MSB 0x03
+#define GYR_Y_LSB 0x04
+#define GYR_Y_MSB 0x05
+#define GYR_Z_LSB 0x06
+#define GYR_Z_MSB 0x07
+ 
+// Accelerometer full-scale ranges
+enum acc_range
+{
+    ACC_RANGE_3G  = 0x00, 
+    ACC_RANGE_6G  = 0x01,
+    ACC_RANGE_12G = 0x02, 
+    ACC_RANGE_24G = 0x03 
+};
+
+// Accelerometer output data rates
+enum acc_odr
+{
+    ACC_ODR_12   = 0x05, 
+    ACC_ODR_25   = 0x06, 
+    ACC_ODR_50   = 0x07, 
+    ACC_ODR_100  = 0x08, 
+    ACC_ODR_200  = 0x09, 
+    ACC_ODR_400  = 0x0A, 
+    ACC_ODR_800  = 0x0B, 
+    ACC_ODR_1600 = 0x0C, 
+};
+ 
+// Gyroscope full-scale ranges
+enum gyr_range
+{
+    GYR_RANGE_2000DPS = 0x00,
+    GYR_RANGE_1000DPS = 0x01,   
+    GYR_RANGE_500DPS  = 0x02,   
+    GYR_RANGE_250DPS  = 0x03,  
+    GYR_RANGE_125DPS  = 0x04, 
+};
+
+// Gyroscope output data rates
+enum gyr_odr
+{
+    GYR_ODR_2000 = 0x01, 
+    GYR_ODR_1000 = 0x02, 
+    GYR_ODR_400  = 0x03, 
+    GYR_ODR_200  = 0x04, 
+    GYR_ODR_100  = 0x05, 
+};
+ 
+// BMI088 (IMU sensor) class
+class BMI088
+{
+    public:
+        // Class constructor
+        BMI088(PinName sda, PinName scl);
+        // Initialize sensor
+        bool init();
+        // Read accelerometer and gyroscope data
+        void read();
+        // Accelerometer data [m/s^2]
+        float ax, ay, az;
+        // Gyroscope data [rad/s]
+        float gx, gy, gz;
+    private:
+        // I2C bus
+        I2C i2c;      
+        // Write data into register on given I2C bus address
+        void write_reg(uint8_t add, uint8_t reg, uint8_t data);
+        // Read data from register on given I2C bus address
+        char read_reg(uint8_t add, uint8_t reg);
+        // Setup I2C bus
+        void setup_i2c();
+        // Test I2C bus
+        bool test_i2c();         
+        // Setup accelerometer
+        void setup_acc(acc_range a_scale = ACC_RANGE_12G, acc_odr a_odr = ACC_ODR_100);
+        // Setup gyroscope
+        void setup_gyr(gyr_range g_scale = GYR_RANGE_500DPS, gyr_odr g_odr = GYR_ODR_2000);
+        // Read accelerometer data
+        void read_acc();
+        // Read gyroscope data
+        void read_gyr();          
+        // Accelerometer resolution [m/s^2 / bit]
+        float a_res;
+        // Gyroscope resolution [rad/s / bit]
+        float g_res;
+        
+};
+ 
+#endif
